d3624ce1a0136fff8e6a9740ab6b58cc2cc22dcf
Kiran <kirank@delta-n.nl>
2022-12-07 13:39:34 +0100

    made the solution more efficient

--- days/day-6/day6.py
+++ days/day-6/day6.py
@@ -1,19 +1,11 @@
 lines = open("D:\AA Projects\AoC-2022\days\day-6\input.txt", "r").read()
 
-# solution 1
-pairsize = 4
-
-for a, b in enumerate(lines):
+# solution 1 + 2
+def findSolution(pairsize):
+  for a, b in enumerate(lines):
     possiblepairs = lines[a:a+pairsize]
     if len(set(possiblepairs)) == pairsize and possiblepairs[0] != '':
-        print('The Aswer to the first problem is:',a + pairsize)
-        break
+        return(a + pairsize)
 
-#solution 2
-pairsize2 = 14
-
-for a, b in enumerate(lines):
-    possiblepairs2 = lines[a:a+pairsize2]
-    if len(set(possiblepairs2)) == pairsize2 and possiblepairs2[0] != '':
-        print('The Aswer to the second problem is:',a + pairsize2)
-        break
+print(findSolution(4))
+print(findSolution(14))
